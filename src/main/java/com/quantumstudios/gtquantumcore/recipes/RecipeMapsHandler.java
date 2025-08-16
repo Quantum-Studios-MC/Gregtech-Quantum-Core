d79f1966587441e6d3bfbc0945a0ca52aa32cf69
iristhepianist <164552050+iristhepianist@users.noreply.github.com>
2025-08-16 21:57:26 +0530

    Update RecipeMapsHandler.java

--- src/main/java/com/quantumstudios/gtquantumcore/recipes/RecipeMapsHandler.java
+++ src/main/java/com/quantumstudios/gtquantumcore/recipes/RecipeMapsHandler.java
@@ -5,7 +5,7 @@ import gregtech.api.recipes.builders.SimpleRecipeBuilder;
 
 public class RecipeMapsHandler 
 {
-	public static final RecipeMap<SimpleRecipeBuilder> ELECTRICAL_SPRENGEL_PUMP_MAP = new RecipeMap<>("electrical_sprengel_pump", 2, 3, 1, 0,
+	public static final RecipeMap<SimpleRecipeBuilder> ELECTRICAL_SPRENGEL_PUMP_MAP = new RecipeMap<>("electrical_sprengel_pump", 3, 1, 1, 0,
 			new SimpleRecipeBuilder().duration(10).EUt(2), false);
 	//		.setSlotOverlay(false, false, false, gregtech.client.renderer.texture.Textures.)
 	//		.setSlotOverlay(true, true, true, gregtech.client.renderer.texture.Textures.SIFTER_OVERLAY)
